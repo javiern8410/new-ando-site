3b1fcb5468c162790f8707df6107138f7adb1bf2
javiern8410 <javier.nieve@despegar.com>
2020-07-23 11:14:47 -0300

    Changes

--- src/components/header/navbar/NavBar.tsx
+++ src/components/header/navbar/NavBar.tsx
@@ -9,21 +9,21 @@ const NavBar = () => {
     const [login, setLogin] = React.useState(false)
 ;    return (
         <Navbar collapseOnSelect expand="lg" bg="dark" sticky='top' variant="dark" className="site-navbar">
-            <Navbar.Brand href="#home"><Logo /></Navbar.Brand>
+            <Navbar.Brand href="/asdasdadsd"><Logo /></Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="mr-auto">
                 </Nav>
                 <Nav>
-                    <Nav.Link href="#deets">More deets</Nav.Link>
+                    <Nav.Link href="/asdadsasd">More deets</Nav.Link>
                     <Nav.Link eventKey={2} href="#memes">
                         Dank memes
                     </Nav.Link>
                     {login ? (
                         <NavDropdown title="John Doe" id="collasible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Dashboard</NavDropdown.Item>
+                            <NavDropdown.Item href="/asdadsada">Dashboard</NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="#" onClick={() => setLogin(false)}>Logout</NavDropdown.Item>
+                            <NavDropdown.Item href="/sdfsdf" onClick={() => setLogin(false)}>Logout</NavDropdown.Item>
                         </NavDropdown>
                     ) : (
                         <Navbar.Text>
